62bbd02962de11a9ebdbbf571ecfbcd82189061d
Faulo <FauloLio@gmail.com>
2018-03-01 18:06:09 +0100

    src

--- assets/data/fetch.php
+++ assets/data/fetch.php
@@ -1,43 +1,40 @@
 <?php
-namespace Slothsoft\Farah;
+namespace Slothsoft\Chat;
 
-use Slothsoft\Chat\Model;
 use Slothsoft\DBMS\DatabaseException;
+use Slothsoft\Farah\Module\FarahUrl\FarahUrl;
 use DOMDocument;
-use Slothsoft\Farah\Module\Assets\AssetInterface;
 
-return function(AssetInterface $asset) {
-    $args = $asset->getArguments();
-    
-    $httpRequest = Kernel::getInstance()->getRequest();
+return function(FarahUrl $url) {
+    $args = $url->getArguments();
     
     $dbName = 'cms';
     $tableName = 'minecraft_log';
     
-    if ($name = $httpRequest->getInputValue('chat-database') and $name !== $tableName) {
+    if ($name = $args->get('chat-database') and $name !== $tableName) {
         $dbName = 'chat';
         $tableName = $name;
     }
     
-    //TOOD: enable mysql
     $dataDoc = new DOMDocument();
-    $retNode = $dataDoc->createElement('range');
-    $retNode->setAttribute('db-name', $dbName);
-    $retNode->setAttribute('db-table', $tableName);
-    $dataDoc->appendChild($retNode);
-    return $dataDoc;
-    
-    $chat = new Model();
-    try {
-        $chat->init($dbName, $tableName);
-    } catch(DatabaseException $e) {
+    //TOOD: enable mysql+chat
+    if ($chatDisabled = true) {
+        $retNode = $dataDoc->createElement('range');
+        $retNode->setAttribute('db-name', $dbName);
+        $retNode->setAttribute('db-table', $tableName);
+        $dataDoc->appendChild($retNode);
+    } else {
+        $chat = new Model();
+        try {
+            $chat->init($dbName, $tableName);
+        } catch(DatabaseException $e) {
+            
+        }
+        $duration = (int) $args->get('chat-duration', 10 * 365);
+        $end = time();
+        $start = $end - $duration * TIME_DAY;
         
+        $dataDoc->appendChild($chat->getRangeNode($start, $end, $dataDoc));
     }
-    $duration = (int) $httpRequest->getInputValue('chat-duration', 10 * 365);
-    $end = $httpRequest->time;
-    $start = $end - $duration * TIME_DAY;
-    
-    $dataDoc = new DOMDocument();
-    $dataDoc->appendChild($chat->getRangeNode($start, $end, $dataDoc));
     return $dataDoc;
 };
--- assets/template/default.xsl
+++ assets/template/default.xsl
@@ -3,16 +3,21 @@
 	xmlns="http://www.w3.org/1999/xhtml"
 	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
 	xmlns:sfm="http://schema.slothsoft.net/farah/module">
- 
+ 	
+ 	<xsl:import href="farah://slothsoft@farah/xsl/module"/>
+ 	
 	<xsl:template match="/*">
-		<xsl:for-each select="*[@name='fetch']">
-			<form data-chat-id="form" data-chat-last-id="{range/@last-id}" data-chat-database="{range/@db-table}">
-				<ul data-chat-id="list" class="messageLog">
-					<xsl:apply-templates select="range/p"/>
-				</ul>
-				<input type="text" data-chat-id="input" autocomplete="off" disabled="disabled" class="myParagraph"/>
-			</form>
-		</xsl:for-each>
+		<div>
+			<xsl:apply-templates select="sfm:error" mode="sfm:html"/>
+			<xsl:for-each select="*[@name='fetch']">
+				<form data-chat-id="form" data-chat-last-id="{range/@last-id}" data-chat-database="{range/@db-table}">
+					<ul data-chat-id="list" class="messageLog">
+						<xsl:apply-templates select="range/p"/>
+					</ul>
+					<input type="text" data-chat-id="input" autocomplete="off" disabled="disabled" class="myParagraph"/>
+				</form>
+			</xsl:for-each>
+		</div>
 	</xsl:template>
 	
 	<xsl:template match="range">
