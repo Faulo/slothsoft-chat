5a0df0a3bb075856454e235a7717a845d1566e74
Daniel Schulz <faulolio@gmail.com>
2018-07-04 22:56:58 +0200

    auto-commit

--- src/Slothsoft/Chat/Assets/FetchBuilder.php
+++ src/Slothsoft/Chat/Assets/FetchBuilder.php
@@ -5,14 +5,14 @@ namespace Slothsoft\Chat\Assets;
 use Slothsoft\Chat\Model;
 use Slothsoft\Core\Calendar\Seconds;
 use Slothsoft\Core\DBMS\DatabaseException;
+use Slothsoft\Core\IO\Writable\Delegates\DOMWriterFromElementDelegate;
 use Slothsoft\Farah\FarahUrl\FarahUrlArguments;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 use Slothsoft\Farah\Module\Asset\ExecutableBuilderStrategy\ExecutableBuilderStrategyInterface;
-use Slothsoft\Farah\Module\DOMWriter\ElementClosureDOMWriter;
 use Slothsoft\Farah\Module\Executable\ExecutableStrategies;
+use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\DOMWriterResultBuilder;
 use DOMDocument;
 use DOMElement;
-use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\DOMWriterResultBuilder;
 
 class FetchBuilder implements ExecutableBuilderStrategyInterface
 {
@@ -38,7 +38,7 @@ class FetchBuilder implements ExecutableBuilderStrategyInterface
         $writer = function (DOMDocument $targetDoc) use ($chat, $start, $end): DOMElement {
             return $chat->getRangeNode($start, $end, $targetDoc);
         };
-        $resultBuilder = new DOMWriterResultBuilder(new ElementClosureDOMWriter($writer));
+        $resultBuilder = new DOMWriterResultBuilder(new DOMWriterFromElementDelegate($writer), 'fetch.xml');
         return new ExecutableStrategies($resultBuilder);
     }
 }
