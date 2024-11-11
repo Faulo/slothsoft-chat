4880c3cf8325a3e535ed898c90e058fb3d7b9e7b
Daniel Schulz <faulolio@gmail.com>
2024-11-11 01:02:10 +0100

    refactor: lint

--- assets/js/Shoutbox.js
+++ assets/js/Shoutbox.js
@@ -2,23 +2,23 @@ import { App } from "./ShoutboxComponents/App";
 import * as Module from "/slothsoft@farah/js/Module";
 
 try {
-	const nodeList = document.querySelectorAll("*[data-chat-id='form']");
-	if (nodeList.length) {
-		Module.resolveToDocument("farah://slothsoft@chat/xsl/form")
-			.then((templateDoc) => {
-				for (let i = 0; i < nodeList.length; i++) {
-					let node = nodeList[i];
-					try {
-						if (node.getBoundingClientRect().height) {
-							new App(node, templateDoc, false);
-						}
-					} catch(e) {
-						console.log(e);
-					}
-				}
-				return templateDoc;
-			});
-	}
-} catch(e) {
-	console.log(e);
+    const nodeList = document.querySelectorAll("*[data-chat-id='form']");
+    if (nodeList.length) {
+        Module.resolveToDocument("farah://slothsoft@chat/xsl/form")
+            .then((templateDoc) => {
+                for (let i = 0; i < nodeList.length; i++) {
+                    let node = nodeList[i];
+                    try {
+                        if (node.getBoundingClientRect().height) {
+                            new App(node, templateDoc, false);
+                        }
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+                return templateDoc;
+            });
+    }
+} catch (e) {
+    console.log(e);
 }
