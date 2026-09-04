8231bd58d9c7fefb73baf52b5a1153e49c7acd84
Daniel Schulz <faulolio@gmail.com>
2026-09-04 23:35:36 +0200

    chore: ddev

--- test-files/index.php
+++ test-files/index.php
@@ -1,3 +1,16 @@
 <?php
+declare(strict_types = 1);
 
-phpinfo();
+use Slothsoft\Farah\Http\MessageFactory;
+use Slothsoft\Farah\Kernel;
+use Slothsoft\Farah\RequestStrategy\LookupAssetStrategy;
+use Slothsoft\Farah\ResponseStrategy\SendHeaderAndBodyStrategy;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$request = MessageFactory::createServerRequest();
+$requestStrategy = new LookupAssetStrategy();
+$responseStrategy = new SendHeaderAndBodyStrategy();
+
+$kernel = Kernel::getInstance();
+$kernel->handle($requestStrategy, $responseStrategy, $request);
